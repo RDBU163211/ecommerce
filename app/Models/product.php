114a817bf445684e2bc3544289518314aa1f24d3
bahiru workineh <workinehbahiru@gmail.com>
2022-04-20 16:45:58 +0300

    comm

--- app/Models/product.php
+++ app/Models/product.php
@@ -1,6 +1,9 @@
+
 <?php
 
 namespace App\Models;
+
+
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,11 +11,12 @@ use Illuminate\Database\Eloquent\Model;
 class product extends Model
 {
     use HasFactory;
-    protected $ fillable = [
-        'id',
-        'name',
-        'unit',
-        'price',
-        'quantity',
-    ]
+    protected $fillable=[
+     'name',
+     'unit',
+     'price',
+     'quantity'
+    ];
 }
+
+
--- database/migrations/2022_04_18_113736_create_products_table.php
+++ database/migrations/2022_04_18_113736_create_products_table.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Database\Migrations\Migration;
@@ -6,27 +7,28 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateProductsTable extends Migration
 {
-
+    /* Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->timestamps();
             $table->string('name');
             $table->string('unit')->nullable();
-            $table->double('price')->nullable();
+            $table->double('price',82)->nullable();
             $table->integer('quantity')->nullable();
-            
         });
     }
 
-    /**
-       * Reverse the migrations.
+    /* Reverse the migrations.
      *
      * @return void
      */
     public function down()
     {
         Schema::dropIfExists('products');
-
     }
 }
