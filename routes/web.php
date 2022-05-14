2f40bd7363627d7c6b6d255938a0d75538320080
bahiru workineh <workinehbahiru@gmail.com>
2022-05-14 22:49:27 +0300

    project change

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+
+use App\Models\product;
+use App\Models\Catagory;
 use Illuminate\Support\Facades\Route;
-use APP\Http\Controllers\product;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CatagoryController;
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,13 +20,25 @@ use APP\Http\Controllers\product;
 |
 */
 
-
 Route::get('/', function () {
-    
     return view('welcome');
 });
+Route::get('/product/register', [ProductController::class, 'register'])->name('product/register');
+Route::post('/product/register', [ProductController::class, 'store'])->name('product/register');
+Route::get('/Catagory/register', [CatagoryController::class, 'register'])->name('Catagory/register');
+Route::post('/Catagory/register', [CatagoryController::class, 'store'])->name('Catagory/register');
+
+
+
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController ::class, 'index'])->name('home');
+Route::get('/product/list', [ProductController::class, 'get_all'])->name('product/list');
+Route::get('/Catagory/get_all', [CatagoryController::class, 'get_all'])->name('Catagory/get_all');
+Route::get('/product/edit/{id}', [ProductController::class, 'edit']);
+Route::post('/product/update',[ProductController::class,'update'])->name('product/update');
+Route::get('/product/delete/{id}', [ProductController::class, 'delete']);
+Route::get('/product/search/{id}', [ProductController::class, 'get_by_id']);
+Route::get('/Catagory/search/{id}', [CatagoryController::class, 'get_by_id']);
 Auth::routes();
 
-Route::post('/home',[app\http\controllers\homecontroller::class,'index'])->name('home');
-Route::get('/product/register',[productcontroller::class, 'register']);
-Route::post('/product.register',[productcontroller::class, 'store']);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
